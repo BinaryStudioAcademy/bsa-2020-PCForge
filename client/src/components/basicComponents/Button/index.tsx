2ddee3c36fa14c9054e7a60f2422240ccb494496
Maryna <marynavatsko@gmail.com>
2020-08-04 13:40:41 +0300

    changed button component before checking

--- client/src/components/basicComponents/Button/index.tsx
+++ client/src/components/basicComponents/Button/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MButton, {ButtonProps} from '@material-ui/core/Button';
 import { getIcon } from 'helpers/icon.helper';
+import styles from './styles.module.scss';
 
 
 interface IButtonProps {
@@ -14,12 +15,25 @@ interface IButtonProps {
 
 
 const Button:React.FC<ButtonProps & IButtonProps> = ({primary, secondary, warning, error, icon, children, ...rest}) => {
+    const classes = [styles.btn]
+    if(primary) {
+        classes.push(styles['btn-primary'])
+    } else if(secondary) {
+        classes.push(styles['btn-secondary'])
+    } else if(warning) {
+        classes.push(styles['btn-warning'])
+        icon = 'Warning'
+    } else if(error) {
+        classes.push(styles['btn-error'])
+        icon = 'Error'
+    }
+    
     return (
         <MButton
             {...rest}
             variant="outlined"
-            color={primary?`primary`:( secondary?`secondary`:`default`)}
             startIcon={icon ? getIcon(icon) : null}
+            className={classes.join(' ')}
         >
             {children}
         </MButton>
--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Routes } from 'common/enums';
-import Link from '../components/basicComponents/Link';
-import Button from '../components/basicComponents/Button';
-import Input from '../components/basicComponents/Input';
 
 export default class App extends Component {
   render() {
-    const toShow = (
-      <>
-      <Link onClick={()=>alert('hi from link')}>Link</Link>
-      <Button onClick={()=>alert('hi from Button')}>Button</Button>
-      <Input onChange={(e)=>alert(e.target.value)}>Input</Input>
-      </>
-    )
     return (
       <Switch>
-        <Route exact path={Routes.DEFAULT} component={() => <div>{toShow}</div>} />
+        <Route exact path={Routes.DEFAULT} component={() => <div></div>} />
       </Switch>
     );
   }
