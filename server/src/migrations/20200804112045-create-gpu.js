c571115698beb5ffa385a9791e0cb676987c4536
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-04 21:12:37 +0300

    database:add gpus migration && model

---
+++ server/src/migrations/20200804112045-create-gpu.js
@@ -0,0 +1,56 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Gpus', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      interface: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      memorySize: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      coreClocks: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      directx: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      opengl: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      tdp: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      performance: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Gpus');
+  },
+};
