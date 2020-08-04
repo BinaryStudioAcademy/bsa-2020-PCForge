2e79b60b5bc037b4b0b98392bcbba436c9bd3c5e
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-04 21:14:59 +0300

    database:add rates migration && model

---
+++ server/src/migrations/20200804123647-create-rate.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Rates', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      ratebleType: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      ratebleId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Rates');
+  },
+};
