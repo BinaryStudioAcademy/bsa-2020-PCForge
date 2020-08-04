89af17afaa6f9e3fc6f708467c750e3ef2183443
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-04 21:10:05 +0300

    database:add sockets migration && model

---
+++ server/src/models/socket.ts
@@ -0,0 +1,40 @@
+import { BuildOptions, DataTypes, Model, Sequelize } from 'sequelize';
+
+export interface SocketAttributes {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export interface SocketModel extends Model<SocketAttributes>, SocketAttributes {}
+export class Socket extends Model<SocketModel, SocketAttributes> {}
+
+export type SocketStatic = typeof Model & {
+  new (values?: Record<string, unknown>, options?: BuildOptions): SocketStatic;
+};
+
+export function SocketFactory(sequelize: Sequelize): SocketStatic {
+  return <SocketStatic>sequelize.define('sockets', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING(50),
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+}
