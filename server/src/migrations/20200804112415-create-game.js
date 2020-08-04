26375e666c72d7d7f4c19644c1067c2f9477ccf9
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-04 21:13:01 +0300

    database:add games migration && model

---
+++ server/src/migrations/20200804112415-create-game.js
@@ -0,0 +1,84 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      year: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      recommendedRamSize: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      minimalRamSize: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      recommendedCpuId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Cpus',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      recommendedGpuId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Gpus',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      minimalCpuId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Cpus',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      minimalGpuId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Gpus',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Games');
+  },
+};
