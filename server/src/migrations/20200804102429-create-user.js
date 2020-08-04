5d8970657e5d1039e5f1a2ebc6adf02b836e6dc6
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-04 21:06:28 +0300

    database:add users migration && model

---
+++ server/src/migrations/20200804102429-create-user.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      isAdmin: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      avatar: {
+        type: Sequelize.STRING(50),
+      },
+      verifyEmailToken: {
+        type: Sequelize.STRING(50),
+      },
+      resetPasswordToken: {
+        type: Sequelize.STRING(50),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  },
+};
---
+++ server/src/models/user.ts
@@ -0,0 +1,65 @@
+import { BuildOptions, DataTypes, Model, Sequelize } from 'sequelize';
+
+export interface UserAttributes {
+  id: number;
+  name: string;
+  password: string;
+  email: string;
+  isAdmin: boolean;
+  avatar: string | null;
+  verifyEmailToken: string | null;
+  resetPasswordToken: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export interface UserModel extends Model<UserAttributes>, UserAttributes {}
+export class User extends Model<UserModel, UserAttributes> {}
+
+export type UserStatic = typeof Model & {
+  new (values?: Record<string, unknown>, options?: BuildOptions): UserModel;
+};
+
+export function UserFactory(sequelize: Sequelize): UserStatic {
+  return <UserStatic>sequelize.define('users', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING(50),
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING(50),
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING(50),
+    },
+    isAdmin: {
+      allowNull: false,
+      type: DataTypes.BOOLEAN,
+    },
+    avatar: {
+      type: DataTypes.STRING(50),
+    },
+    verifyEmailToken: {
+      type: DataTypes.STRING(50),
+    },
+    resetPasswordToken: {
+      type: DataTypes.STRING(50),
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+}
