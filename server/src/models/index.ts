444df1820e8a04f20e9a04e8f3561c3256f03766
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-04 21:15:56 +0300

    database:add reations between models

---
+++ server/src/models/index.ts
@@ -0,0 +1,107 @@
+import orm from '../db/connection';
+
+import { CommentFactory } from './comment';
+import { CpuFactory } from './cpu';
+import { GameFactory } from './game';
+import { GpuFactory } from './gpu';
+import { MotherboardFactory } from './motherboard';
+import { NewsFactory } from './news';
+import { PowerSupplyFactory } from './powersupply';
+import { RamFactory } from './ram';
+import { RamTypeFactory } from './ramtype';
+import { RateFactory } from './rate';
+import { SetupFactory } from './setup';
+import { SocketFactory } from './socket';
+import { UserFactory } from './user';
+import { UserGameFactory } from './usergame';
+
+const CommentModel = CommentFactory(orm);
+const CpuModel = CpuFactory(orm);
+const GameModel = GameFactory(orm);
+const GpuModel = GpuFactory(orm);
+const MotherboardModel = MotherboardFactory(orm);
+const NewsModel = NewsFactory(orm);
+const PowerSupplyModel = PowerSupplyFactory(orm);
+const RamModel = RamFactory(orm);
+const RamTypeModel = RamTypeFactory(orm);
+const RateModel = RateFactory(orm);
+const SetupModel = SetupFactory(orm);
+const SocketModel = SocketFactory(orm);
+const UserModel = UserFactory(orm);
+const UserGameModel = UserGameFactory(orm);
+
+CommentModel.belongsTo(UserModel);
+CommentModel.belongsTo(SetupModel, { foreignKey: 'commentable_id', constraints: false });
+CommentModel.belongsTo(NewsModel, { foreignKey: 'commentable_id', constraints: false });
+CommentModel.belongsTo(GameModel, { foreignKey: 'commentable_id', constraints: false });
+
+CpuModel.belongsTo(SocketModel);
+CpuModel.hasMany(SetupModel);
+CpuModel.hasMany(GameModel);
+
+GameModel.hasMany(RateModel, { foreignKey: 'rateble_id', constraints: false });
+GameModel.hasMany(CommentModel, { foreignKey: 'commentable_id', constraints: false });
+GameModel.belongsTo(CpuModel, { as: 'minimalCpuId' });
+GameModel.belongsTo(CpuModel, { as: 'recommendedCpuId' });
+GameModel.belongsTo(GpuModel, { as: 'minimalGpuId' });
+GameModel.belongsTo(GpuModel, { as: 'recommendedGpuId' });
+GameModel.hasMany(UserGameModel);
+
+GpuModel.hasMany(SetupModel);
+GpuModel.hasMany(GameModel);
+
+MotherboardModel.belongsTo(SocketModel);
+MotherboardModel.belongsTo(RamTypeModel);
+
+NewsModel.hasMany(RateModel, { foreignKey: 'rateble_id', constraints: false });
+NewsModel.hasMany(CommentModel, { foreignKey: 'commentable_id', constraints: false });
+
+PowerSupplyModel.hasMany(SetupModel);
+
+RamModel.belongsTo(RamTypeModel);
+RamModel.hasMany(SetupModel);
+
+RamTypeModel.hasMany(RamModel);
+RamTypeModel.hasMany(MotherboardModel);
+
+RateModel.belongsTo(UserModel);
+RateModel.belongsTo(SetupModel, { foreignKey: 'rateble_id', constraints: false });
+RateModel.belongsTo(NewsModel, { foreignKey: 'rateble_id', constraints: false });
+RateModel.belongsTo(GameModel, { foreignKey: 'rateble_id', constraints: false });
+
+SetupModel.belongsTo(UserModel);
+SetupModel.belongsTo(CpuModel);
+SetupModel.belongsTo(GpuModel);
+SetupModel.belongsTo(RamModel);
+SetupModel.belongsTo(MotherboardModel);
+SetupModel.belongsTo(PowerSupplyModel);
+SetupModel.hasMany(CommentModel, { foreignKey: 'commentable_id', constraints: false });
+SetupModel.hasMany(RateModel, { foreignKey: 'rateble_id', constraints: false });
+
+SocketModel.hasMany(MotherboardModel);
+SocketModel.hasMany(CpuModel);
+
+UserModel.hasMany(RateModel);
+UserModel.hasMany(CommentModel);
+UserModel.hasMany(SetupModel);
+UserModel.hasMany(UserGameModel);
+
+UserGameModel.belongsTo(UserModel);
+UserGameModel.belongsTo(GameModel);
+
+// eslint-disable-next-line prettier/prettier
+export {
+  CommentModel,
+  CpuModel,
+  GameModel,
+  GpuModel,
+  MotherboardModel,
+  NewsModel,
+  PowerSupplyModel,
+  RamModel,
+  RamTypeModel,
+  SetupModel,
+  SocketModel,
+  UserModel,
+  UserGameModel,
+};
