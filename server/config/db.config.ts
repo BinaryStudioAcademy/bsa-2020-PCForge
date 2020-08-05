2cc7ee846f69a3a433fdd2632a3bc1e86fb8e94a
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-05 14:17:27 +0300

    database: update config file && update imports in connection.ts

--- server/config/db.config.ts
+++ server/config/db.config.ts
@@ -2,12 +2,14 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports = {
+export const config = {
   database: process.env.POSTGRES_DB,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
-  logging: console.log,
+  params: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: console.log,
+  },
 };
