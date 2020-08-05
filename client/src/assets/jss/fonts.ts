52f2879b43f6bf110b5ce135b559a2ff050c7ae8
sterletskiy <sterletskiy@artelogic.net>
2020-08-05 08:08:11 +0300

    import Lato font. add font-face rules. add custom colors

---
+++ client/src/assets/jss/fonts.ts
@@ -0,0 +1,100 @@
+import LatoBlack from 'assets/fonts/Lato/Lato-Black.ttf';
+import LatoBlackItalic from 'assets/fonts/Lato/Lato-BlackItalic.ttf';
+import LatoBold from 'assets/fonts/Lato/Lato-Bold.ttf';
+import LatoBoldItalic from 'assets/fonts/Lato/Lato-BoldItalic.ttf';
+import LatoItalic from 'assets/fonts/Lato/Lato-Italic.ttf';
+import LatoLight from 'assets/fonts/Lato/Lato-Light.ttf';
+import LatoLightItalic from 'assets/fonts/Lato/Lato-LightItalic.ttf';
+import LatoRegular from 'assets/fonts/Lato/Lato-Regular.ttf';
+import LatoThin from 'assets/fonts/Lato/Lato-Thin.ttf';
+import LatoThinItalic from 'assets/fonts/Lato/Lato-ThinItalic.ttf';
+
+export const latoBlack = {
+  fontFamily: 'LatoBlack',
+  src: `
+    local('Lato'),
+    local('Lato-Black'),
+    url(${LatoBlack}) format('ttf')
+  `,
+};
+
+export const latoBlackItalic = {
+  fontFamily: 'LatoBlackItalic',
+  src: `
+    local('Lato'),
+    local('Lato-BlackItalic'),
+    url(${LatoBlackItalic}) format('ttf')
+  `,
+};
+
+export const latoBold = {
+  fontFamily: 'LatoBold',
+  src: `
+    local('Lato'),
+    local('Lato-Bold'),
+    url(${LatoBold}) format('ttf')
+  `,
+};
+
+export const latoBoldItalic = {
+  fontFamily: 'LatoBoldItalic',
+  src: `
+    local('Lato'),
+    local('Lato-BoldItalic'),
+    url(${LatoBoldItalic}) format('ttf')
+  `,
+};
+
+export const latoItalic = {
+  fontFamily: 'LatoItalic',
+  src: `
+    local('Lato'),
+    local('Lato-Italic'),
+    url(${LatoItalic}) format('ttf')
+  `,
+};
+
+export const latoLight = {
+  fontFamily: 'LatoLight',
+  src: `
+    local('Lato'),
+    local('Lato-Light'),
+    url(${LatoLight}) format('ttf')
+  `,
+};
+
+export const latoLightItalic = {
+  fontFamily: 'LatoLightItalic',
+  src: `
+    local('Lato'),
+    local('Lato-LightItalic'),
+    url(${LatoLightItalic}) format('ttf')
+  `,
+};
+
+export const latoRegular = {
+  fontFamily: 'LatoRegular',
+  src: `
+    local('Lato'),
+    local('Lato-Regular'),
+    url(${LatoRegular}) format('ttf')
+  `,
+};
+
+export const latoThin = {
+  fontFamily: 'LatoThin',
+  src: `
+    local('Lato'),
+    local('Lato-Thin'),
+    url(${LatoThin}) format('ttf')
+  `,
+};
+
+export const latoThinItalic = {
+  fontFamily: 'LatoThinItalic',
+  src: `
+    local('Lato'),
+    local('Lato-ThinItalic'),
+    url(${LatoThinItalic}) format('ttf')
+  `,
+};
