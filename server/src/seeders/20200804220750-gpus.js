64a55656105158dd11def18eaa9d4d1116e1f01c
Evgeniy <evgeny.kasyan@outlook.com>
2020-08-05 23:47:49 +0300

    Create seeder for GPUs

---
+++ server/src/seeders/20200804220750-gpus.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const gpus = require("../seed-data/gpus");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    try {
+      return await queryInterface.bulkInsert('gpus', gpus.map(gpu => {
+        return {
+          ...gpu,
+          createdAt: new Date(Date.now()),
+          updatedAt: new Date(Date.now())
+        }
+      }), {});
+    }
+    catch (err) {
+      console.log(`Seeding error: ${err}`);
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    try {
+      await queryInterface.bulkDelete('gpus', null, {});
+    } catch (err) {
+      console.log(`Seeding error: ${err}`);
+    }
+  }
+};
