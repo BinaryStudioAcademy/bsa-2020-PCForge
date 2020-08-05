6f40769249fdbbd32666f7654d69d3bff564af46
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-05 14:13:11 +0300

    database: update connection

--- server/src/data/db/connection.ts
+++ server/src/data/db/connection.ts
@@ -1,4 +1,57 @@
 import Sequelize from 'sequelize';
 import * as config from '../../../config/db.config';
 
-export default new Sequelize(config);
+import { CommentStatic } from '../models/comment';
+import { CpuStatic } from '../models/cpu';
+import { GameStatic } from '../models/game';
+import { GpuStatic } from '../models/gpu';
+import { MotherboardStatic } from '../models/motherboard';
+import { NewsStatic } from '../models/news';
+import { PowerSupplyStatic } from '../models/powersupply';
+import { RamStatic } from '../models/ram';
+import { RamTypeStatic } from '../models/ramtype';
+import { RateStatic } from '../models/rate';
+import { SetupStatic } from '../models/setup';
+import { SocketStatic } from '../models/socket';
+import { UserStatic } from '../models/user';
+import { UserGameStatic } from '../models/usergame';
+import { initializeModels } from '../models/index';
+
+export interface Models {
+  Comment: CommentStatic;
+  Cpu: CpuStatic;
+  Game: GameStatic;
+  Gpu: GpuStatic;
+  Motherboard: MotherboardStatic;
+  News: NewsStatic;
+  PowerSupply: PowerSupplyStatic;
+  Ram: RamStatic;
+  RamType: RamTypeStatic;
+  Rate: RateStatic;
+  Setup: SetupStatic;
+  Socket: SocketStatic;
+  User: UserStatic;
+  UserGame: UserGameStatic;
+}
+
+export interface Db {
+  models: Models;
+}
+
+export default fp(async (fastify, opts, next) => {
+  const { database, username, password, params } = config;
+  const sequelize = new Sequelize(database, username, password, params);
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    const models: Models = initializeModels(sequelize);
+
+    fastify.decorate('db', { models });
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+  } finally {
+    next();
+  }
+});
--- server/src/data/models/index.ts
+++ server/src/data/models/index.ts
@@ -1,5 +1,3 @@
-import orm from '../db/connection';
-
 import { CommentFactory } from './comment';
 import { CpuFactory } from './cpu';
 import { GameFactory } from './game';
@@ -15,91 +13,94 @@ import { SocketFactory } from './socket';
 import { UserFactory } from './user';
 import { UserGameFactory } from './usergame';
 
-const CommentModel = CommentFactory(orm);
-const CpuModel = CpuFactory(orm);
-const GameModel = GameFactory(orm);
-const GpuModel = GpuFactory(orm);
-const MotherboardModel = MotherboardFactory(orm);
-const NewsModel = NewsFactory(orm);
-const PowerSupplyModel = PowerSupplyFactory(orm);
-const RamModel = RamFactory(orm);
-const RamTypeModel = RamTypeFactory(orm);
-const RateModel = RateFactory(orm);
-const SetupModel = SetupFactory(orm);
-const SocketModel = SocketFactory(orm);
-const UserModel = UserFactory(orm);
-const UserGameModel = UserGameFactory(orm);
+export const initializeModels = (orm) => {
+  const CommentModel = CommentFactory(orm);
+  const CpuModel = CpuFactory(orm);
+  const GameModel = GameFactory(orm);
+  const GpuModel = GpuFactory(orm);
+  const MotherboardModel = MotherboardFactory(orm);
+  const NewsModel = NewsFactory(orm);
+  const PowerSupplyModel = PowerSupplyFactory(orm);
+  const RamModel = RamFactory(orm);
+  const RamTypeModel = RamTypeFactory(orm);
+  const RateModel = RateFactory(orm);
+  const SetupModel = SetupFactory(orm);
+  const SocketModel = SocketFactory(orm);
+  const UserModel = UserFactory(orm);
+  const UserGameModel = UserGameFactory(orm);
 
-CommentModel.belongsTo(UserModel);
-CommentModel.belongsTo(SetupModel, { foreignKey: 'commentableId', constraints: false });
-CommentModel.belongsTo(NewsModel, { foreignKey: 'commentableId', constraints: false });
-CommentModel.belongsTo(GameModel, { foreignKey: 'commentableId', constraints: false });
+  CommentModel.belongsTo(UserModel);
+  CommentModel.belongsTo(SetupModel, { foreignKey: 'commentableId', constraints: false });
+  CommentModel.belongsTo(NewsModel, { foreignKey: 'commentableId', constraints: false });
+  CommentModel.belongsTo(GameModel, { foreignKey: 'commentableId', constraints: false });
 
-CpuModel.belongsTo(SocketModel);
-CpuModel.hasMany(SetupModel);
+  CpuModel.belongsTo(SocketModel);
+  CpuModel.hasMany(SetupModel);
 
-GameModel.hasMany(RateModel, { foreignKey: 'ratebleId', constraints: false });
-GameModel.hasMany(CommentModel, { foreignKey: 'commentableId', constraints: false });
-GameModel.belongsTo(CpuModel, { as: 'minimalCpu' });
-GameModel.belongsTo(CpuModel, { as: 'recommendedCpu' });
-GameModel.belongsTo(GpuModel, { as: 'minimalGpu' });
-GameModel.belongsTo(GpuModel, { as: 'recommendedGpu' });
-GameModel.hasMany(UserGameModel);
+  GameModel.hasMany(RateModel, { foreignKey: 'ratebleId', constraints: false });
+  GameModel.hasMany(CommentModel, { foreignKey: 'commentableId', constraints: false });
+  GameModel.belongsTo(CpuModel, { as: 'minimalCpu' });
+  GameModel.belongsTo(CpuModel, { as: 'recommendedCpu' });
+  GameModel.belongsTo(GpuModel, { as: 'minimalGpu' });
+  GameModel.belongsTo(GpuModel, { as: 'recommendedGpu' });
+  GameModel.hasMany(UserGameModel);
 
-GpuModel.hasMany(SetupModel);
+  GpuModel.hasMany(SetupModel);
 
-MotherboardModel.belongsTo(SocketModel);
-MotherboardModel.belongsTo(RamTypeModel);
+  MotherboardModel.belongsTo(SocketModel);
+  MotherboardModel.belongsTo(RamTypeModel);
 
-NewsModel.hasMany(RateModel, { foreignKey: 'ratebleId', constraints: false });
-NewsModel.hasMany(CommentModel, { foreignKey: 'commentableId', constraints: false });
+  NewsModel.hasMany(RateModel, { foreignKey: 'ratebleId', constraints: false });
+  NewsModel.hasMany(CommentModel, { foreignKey: 'commentableId', constraints: false });
 
-PowerSupplyModel.hasMany(SetupModel);
+  PowerSupplyModel.hasMany(SetupModel);
 
-RamModel.belongsTo(RamTypeModel);
-RamModel.hasMany(SetupModel);
+  RamModel.belongsTo(RamTypeModel);
+  RamModel.hasMany(SetupModel);
 
-RamTypeModel.hasMany(RamModel);
-RamTypeModel.hasMany(MotherboardModel);
+  RamTypeModel.hasMany(RamModel);
+  RamTypeModel.hasMany(MotherboardModel);
 
-RateModel.belongsTo(UserModel);
-RateModel.belongsTo(SetupModel, { foreignKey: 'ratebleId', constraints: false });
-RateModel.belongsTo(NewsModel, { foreignKey: 'ratebleId', constraints: false });
-RateModel.belongsTo(GameModel, { foreignKey: 'ratebleId', constraints: false });
+  RateModel.belongsTo(UserModel);
+  RateModel.belongsTo(SetupModel, { foreignKey: 'ratebleId', constraints: false });
+  RateModel.belongsTo(NewsModel, { foreignKey: 'ratebleId', constraints: false });
+  RateModel.belongsTo(GameModel, { foreignKey: 'ratebleId', constraints: false });
 
-SetupModel.belongsTo(UserModel);
-SetupModel.belongsTo(CpuModel);
-SetupModel.belongsTo(GpuModel);
-SetupModel.belongsTo(RamModel);
-SetupModel.belongsTo(MotherboardModel);
-SetupModel.belongsTo(PowerSupplyModel);
-SetupModel.hasMany(CommentModel, { foreignKey: 'commentableId', constraints: false });
-SetupModel.hasMany(RateModel, { foreignKey: 'ratebleId', constraints: false });
+  SetupModel.belongsTo(UserModel);
+  SetupModel.belongsTo(CpuModel);
+  SetupModel.belongsTo(GpuModel);
+  SetupModel.belongsTo(RamModel);
+  SetupModel.belongsTo(MotherboardModel);
+  SetupModel.belongsTo(PowerSupplyModel);
+  SetupModel.hasMany(CommentModel, { foreignKey: 'commentableId', constraints: false });
+  SetupModel.hasMany(RateModel, { foreignKey: 'ratebleId', constraints: false });
 
-SocketModel.hasMany(MotherboardModel);
-SocketModel.hasMany(CpuModel);
+  SocketModel.hasMany(MotherboardModel);
+  SocketModel.hasMany(CpuModel);
 
-UserModel.hasMany(RateModel);
-UserModel.hasMany(CommentModel);
-UserModel.hasMany(SetupModel);
-UserModel.hasMany(UserGameModel);
+  UserModel.hasMany(RateModel);
+  UserModel.hasMany(CommentModel);
+  UserModel.hasMany(SetupModel);
+  UserModel.hasMany(UserGameModel);
 
-UserGameModel.belongsTo(UserModel);
-UserGameModel.belongsTo(GameModel);
+  UserGameModel.belongsTo(UserModel);
+  UserGameModel.belongsTo(GameModel);
 
-// eslint-disable-next-line prettier/prettier
-export {
-  CommentModel,
-  CpuModel,
-  GameModel,
-  GpuModel,
-  MotherboardModel,
-  NewsModel,
-  PowerSupplyModel,
-  RamModel,
-  RamTypeModel,
-  SetupModel,
-  SocketModel,
-  UserModel,
-  UserGameModel,
+  // eslint-disable-next-line prettier/prettier
+  return {
+    Comment: CommentModel,
+    Cpu: CpuModel,
+    Game: GameModel,
+    Gpu: GpuModel,
+    Motherboard: MotherboardModel,
+    News: NewsModel,
+    PowerSupply: PowerSupplyModel,
+    Ram: RamModel,
+    RamType: RamTypeModel,
+    Setup: SetupModel,
+    Socket: SocketModel,
+    User: UserModel,
+    UserGame: UserGameModel,
+    Rate: RateModel,
+  };
 };
