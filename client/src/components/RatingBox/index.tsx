20df719a9b403aa77ee99b80f90c07968298892f
Cool <yuliakulinichsoft@gmail.com>
2020-08-05 19:26:01 +0300

    Update code after code-review
    One function for disabled and editing ratingbar.
    labels as enum.
    Update help informatin in labels.
    Solve warning in console after changing own rate.
    Show help in tooltip.
    Show decimal value of rating (step = 0.1) in tooltip and rating box for disabled rate.
    Change margin-bottom for rating box.
    Change color for empty icon state.

--- client/src/components/RatingBox/index.tsx
+++ client/src/components/RatingBox/index.tsx
@@ -3,6 +3,7 @@ import { withStyles, makeStyles, createStyles } from "@material-ui/core/styles";
 import Rating from "@material-ui/lab/Rating";
 import Box from "@material-ui/core/Box";
 import RatingIcon from './RatingIcon/index';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const StyledRating = withStyles({
   iconFilled: {
@@ -10,76 +11,64 @@ const StyledRating = withStyles({
   },
   iconHover: {
     color: "#EB3D55"
+  },
+  iconEmpty: {
+    opacity: 0.2,
+    color: "#EB3D55"
   }
 })(Rating);
 
 const useStyles = makeStyles(() =>
   createStyles({
-    rateHelp: {
-      position: 'absolute',
-      fontStyle: "italic"
-    },
-    disableRating: {
-      opacity: 0.5
+    fieldset: {
+      display: 'inline-block',
     }
   })
 );
 
-const labels: { [index: string]: string } = {
-  0: "",
-  0.5: "Useless",
-  1: "Useless+",
-  1.5: "Poor",
-  2: "Poor+",
-  2.5: "Ok",
-  3: "Ok+",
-  3.5: "Good",
-  4: "Good+",
-  4.5: "Excellent",
-  5: "Excellent+"
-};
+enum labels {
+  'Useless' = 0.5,
+  'Better that just useless' = 1,
+  'Poor' = 1.5,
+  'Better that just poor' = 2,
+  'Ok' = 2.5,
+  'Better that just ok' = 3,
+  'Good' = 3.5,
+  'Better that just good' = 4,
+  'Excellent' = 4.5,
+  'Better that just excellent' = 5
+}
 
-export function RatingBoxEditable(ratingValue: number) {
+export function RatingBox(ratingValue: number, disabled: boolean) {
   const [value, setValue] = React.useState<number>(ratingValue);
   const [hover, setHover] = React.useState<number>(-1);
   const classes = useStyles();
 
+  const tooltipValue = disabled
+                        ? value
+                        : labels[hover !== -1 ? hover : 0.5];
   return (
-    <div>
-      <Box component="fieldset" mb={3} borderColor="transparent">
-        <StyledRating
-          name="customized-color"
-          defaultValue={value}
-          precision={0.5}
-          icon={<RatingIcon />}
-          onChange={(event: React.ChangeEvent<{}>, newValue: number | null) => {
-            if(newValue)
-            setValue(newValue);
-          }}
-          onChangeActive={(event: React.ChangeEvent<{}>, newHover) => {
-            setHover(newHover);
-          }}
-        />
-        <Box className={classes.rateHelp} >{labels[hover !== -1 ? hover : 0]}</Box>
+      <Tooltip title={tooltipValue} placement="right-start" arrow>
+      <Box mb={1} borderColor="transparent" className={classes.fieldset}>
+          <StyledRating
+            name="customized-color"
+            value={value}
+            disabled={disabled}
+            precision={ disabled ? 0.1 : 0.5 }
+            icon={<RatingIcon />}
+            size="small"
+            onChange={(event: React.ChangeEvent<{}>, newValue: number | null) => {
+              if(newValue) {
+                setValue(newValue);
+                //add function that change reating value
+              }
+            }}
+            onChangeActive={(event: React.ChangeEvent<{}>, newHover) => {
+              setHover(newHover);
+            }}
+          />
       </Box>
-    </div>
+      </Tooltip>
   );
 }
 
-export function RatingBoxReadOnly(ratingValue: number | undefined) {
-  const classes = useStyles();
-
-  return (
-    <div>
-      <Box className={classes.disableRating} component="fieldset" mb={3} borderColor="transparent">
-        <StyledRating
-          name="customized-color"
-          defaultValue={ratingValue}
-          precision={0.5}
-          icon={<RatingIcon />}
-          readOnly
-        />
-      </Box>
-    </div>
-  );
-}
