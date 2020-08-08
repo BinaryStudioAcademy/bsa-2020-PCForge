51f81bac2432fb29c2cc28edd95ed83e0bbe8380
Evgeniy <evgeny.kasyan@outlook.com>
2020-08-08 14:22:27 +0300

    Add functionality to compare passwords wheen user login

--- server/src/api/routes/auth.router.ts
+++ server/src/api/routes/auth.router.ts
@@ -1,21 +1,41 @@
 import { FastifyInstance } from 'fastify';
+import { error } from 'console';
+import { bcrypt } from 'bcrypt';
+const bcrypt = require('bcrypt');
 
 export function router(fastify: FastifyInstance, opts, next): void {
+  const { UserService } = fastify.services;
+
   fastify.post('/auth/login', {}, async (request, reply) => {
-    const { login, password } = request.body;
-    if (!login || !password) {
+    console.log(request.session);
+    try {
+      const { login, password } = request.body;
+      if (login && password) {
+        const User = await UserService.getUserByLoginOrEmail(login);
+        const isPasswordValidForUser = await bcrypt.compare(password, User.password);
+        if (isPasswordValidForUser) {
+          const token = fastify.jwt.sign({}, { expiresIn: 86400 });
+          console.log('functionrouter -> token', token);
+          reply.send(token);
+        } else {
+          reply.status(401).send({
+            error: true,
+            msg: 'Incorrect login or password, please try again',
+          });
+        }
+      } else {
+        throw error;
+      }
+    } catch (error) {
+      console.log(error);
       reply.status(400).send({
         error: true,
         msg: 'Mandatory fields are missing',
       });
     }
-    //HERE MUST BE IMPLEMENTED LOGIC WITH DATABASE
-    const token = fastify.jwt.sign({}, { expiresIn: 86400 });
-    reply.send({ token });
   });
 
   fastify.get('/auth/google/callback', {}, async function (req, res) {
-    // Fastify instance gets decorated with this method on OAuth plugin initialization
     const token = await fastify.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(req);
     res.send({ token: token.access_token });
   });
---
+++ server/src/data/repositories/user.Repository.ts
@@ -0,0 +1,16 @@
+import { UserModel, UserStatic } from '../models/user';
+import { Op } from 'sequelize';
+
+export class UserRepository {
+  constructor(private model: UserStatic) {}
+
+  async getUserByUserNameOrEmail(login: string): Promise<UserModel> {
+    const user = await this.model.findOne({
+      where: {
+        [Op.or]: [{ name: login }, { email: login }],
+      },
+    });
+    // console.log('UserRepository -> constructor -> user', user);
+    return user;
+  }
+}
