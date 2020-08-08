f6302bb8268c0387393c5fc5c549110595f4e471
sterletskiy <sterletskiy@artelogic.net>
2020-08-08 15:36:53 +0300

    upd

--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Routes } from 'common/enums';
 import Login from 'containers/Auth';
-import Title from 'components/Title';
 
 export default class App extends Component {
   render(): JSX.Element {
