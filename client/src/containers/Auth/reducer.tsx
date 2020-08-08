4c95e69375374579e9ac1e366efb15679da7cae4
Lesiv Olexandr <lesivolexandr@gmail.com>
2020-08-08 13:34:54 +0300

    Restore package.json files

---
+++ client/src/containers/Auth/reducer.tsx
@@ -0,0 +1,59 @@
+import { IAuthState } from 'containers/Auth/interfaces';
+import {
+  AuthActionTypes,
+  AUTH_CHANGE_EMAIL,
+  AUTH_CHANGE_PASSWORD,
+  AUTH_VALIDATION_ERROR,
+  AUTH_KEEP_SIGN_IN,
+  AUTH_LOADING_STATUS,
+  AUTH_SWITCH_AUTH_PAGE,
+} from './actionTypes';
+
+const initialState: IAuthState = {
+  email: '',
+  password: '',
+  errorMessage: '',
+  isRegistration: false,
+  keepSignedIn: false,
+  isLoading: false,
+};
+
+export function AuthReducer(state: IAuthState = initialState, action: AuthActionTypes): IAuthState {
+  switch (action.type) {
+    case AUTH_CHANGE_EMAIL:
+      return {
+        ...state,
+        email: action.payload.value,
+      };
+    case AUTH_CHANGE_PASSWORD:
+      return {
+        ...state,
+        password: action.payload.value,
+      };
+    case AUTH_VALIDATION_ERROR:
+      return {
+        ...state,
+        errorMessage: action.payload.errorMessage,
+      };
+    case AUTH_KEEP_SIGN_IN:
+      return {
+        ...state,
+        keepSignedIn: action.payload.keepSignedIn,
+      };
+    case AUTH_LOADING_STATUS:
+      return {
+        ...state,
+        isLoading: action.payload.isLoading,
+      };
+    case AUTH_SWITCH_AUTH_PAGE:
+      return {
+        ...state,
+        email: '',
+        password: '',
+        errorMessage: '',
+        isRegistration: action.payload.isRegistration,
+      };
+    default:
+      return state;
+  }
+}
