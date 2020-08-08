d21da70a000b324319eaaaaac52660141a30a747
sterletskiy <sterletskiy@artelogic.net>
2020-08-08 14:43:18 +0300

    add title component

---
+++ client/src/components/Title/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Title = () => (
+  <div>
+    <h2>Title</h2>
+    <span>Sub title</span>
+  </div>
+);
+
+export default Title;
--- client/src/containers/App.tsx
+++ client/src/containers/App.tsx
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Routes } from 'common/enums';
 import Login from 'containers/Auth';
+import Title from 'components/Title';
 
 export default class App extends Component {
   render(): JSX.Element {
     return (
       <Switch>
-        <Route exact path={Routes.DEFAULT} component={() => <div />} />
+        <Route
+          exact
+          path={Routes.DEFAULT}
+          component={() => (
+            <div>
+              <Title />
+            </div>
+          )}
+        />
         <Route exact path={Routes.LOGIN} component={Login} />
       </Switch>
     );
