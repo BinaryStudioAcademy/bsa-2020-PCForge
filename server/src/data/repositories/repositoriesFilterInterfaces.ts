c2f79bc502b2369968935f69bdd6de219087daa4
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-10 16:49:55 +0300

    server: update repositories filter names && update ram repository to handle filters

--- server/src/data/repositories/repositoriesFilterInterfaces.ts
+++ server/src/data/repositories/repositoriesFilterInterfaces.ts
@@ -10,12 +10,34 @@ export const FilterDefaults: IFilter = {
   count: 50,
 };
 
-export interface ISocketFilter extends IFilter {
+export interface ICpuFilter extends IFilter {
   socketId: string | Record<string, unknown>;
 }
 
-export const SocketFilterDefaults: ISocketFilter = {
+export const CpuFilterDefaults: ICpuFilter = {
   from: 0,
   count: 50,
+  socketId: notNull,
+};
+
+export interface IRamFilter extends IFilter {
+  typeId: string | Record<string, unknown>;
+}
+
+export const RamFilterDefaults: IRamFilter = {
+  from: 0,
+  count: 50,
+  typeId: notNull,
+};
+
+export interface IMotherboardFilter extends IFilter {
+  ramTypeId: string | Record<string, unknown>;
+  socketId: string | Record<string, unknown>;
+}
+
+export const MotherboardFilterDefaults: IMotherboardFilter = {
+  from: 0,
+  count: 50,
+  ramTypeId: notNull,
   socketId: notNull,
 };
