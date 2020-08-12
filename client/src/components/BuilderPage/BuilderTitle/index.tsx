4f4a030b062c89fc4f6e3a7f285814dcb3885cfe
Vismmax <vismmax@gmail.com>
2020-08-13 01:46:30 +0300

    Added reset to title

--- client/src/components/BuilderPage/BuilderTitle/index.tsx
+++ client/src/components/BuilderPage/BuilderTitle/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import { Box } from '@material-ui/core';
+import Button from 'components/BasicComponents/Button';
 
-const BuilderTitle = (): JSX.Element => {
-  return <h1 className={styles.title}>Build</h1>;
+type PropsType = {
+  showReset?: boolean;
+  onReset: () => void;
+};
+
+const BuilderTitle = ({ showReset = false, onReset }: PropsType): JSX.Element => {
+  return (
+    <Box className={styles.builderTitle}>
+      <h1 className={styles.title}>Build</h1>
+      {showReset && <Button onClick={onReset}>Reset Setup</Button>}
+    </Box>
+  );
 };
 
 export default BuilderTitle;
