2090b730f75471a79e437ede7a56ff0c20ad4486
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-13 20:56:42 +0300

    server: removed where filter from base repository

--- server/src/data/repositories/base.repository.ts
+++ server/src/data/repositories/base.repository.ts
@@ -24,23 +24,26 @@ export abstract class BaseRepository<M extends Model, F extends IFilter = IFilte
     return count;
   }
 
-  async getAll(filter?: F, params?: FindOptions): Promise<IWithMeta<M>> {
-    const { from: offset, count: limit, ...uniqueFilters } = reduceTo<F>(filter, this.filterFactory);
+  async getAll(params?: FindOptions, inputFilter?: F): Promise<IWithMeta<M>> {
+    const filter = inputFilter || new this.filterFactory();
+    const reducedFilter = reduceTo<F>(filter, this.filterFactory);
+    const { from: offset, count: limit } = reducedFilter;
 
-    const result = await this._model.findAll({
+    const result = await this._model.findAndCountAll({
       order: [['id', 'ASC']],
-      where: uniqueFilters,
       offset: offset,
       limit: limit,
       ...params,
     });
 
     const globalCount = await this.getCount();
-    const countAfterFiltering = await this.getCount({ ...uniqueFilters });
+    // here is a bug in sequelize: it returns array instead of number, so we use length
+    // https://github.com/sequelize/sequelize/issues/9109
+    const countAfterFiltering = ((result.count as unknown) as Record<string, unknown>[]).length;
 
     return {
       meta: { globalCount, countAfterFiltering },
-      data: result as M[],
+      data: result.rows as M[],
     };
   }
 
