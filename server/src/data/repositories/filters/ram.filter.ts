b590b301e9c52c3f12514e3a9889d253d3a2089c
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-13 22:19:33 +0300

    server: update Ram filter schema

--- server/src/data/repositories/filters/ram.filter.ts
+++ server/src/data/repositories/filters/ram.filter.ts
@@ -1,9 +1,14 @@
 import { IFilter } from './base.filter';
 import { IRamTypeFilter } from './ramType.filter';
+import { FilterRangeType } from './types';
 
 export class IRamFilter extends IFilter {
   constructor() {
     super();
   }
-  type: IRamTypeFilter;
+  type: IRamTypeFilter = new IRamTypeFilter();
+  memorySize: FilterRangeType<number> = {
+    minValue: 0,
+    maxValue: 100,
+  };
 }
