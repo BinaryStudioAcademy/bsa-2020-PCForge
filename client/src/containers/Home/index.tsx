0d9c20832117669814c1e1e7b317433f73fb58cc
Mikhail Tseluiko <mikhail.tseluiko@binary-studio.com>
2020-08-13 09:51:27 +0300

    Main page: add dummy page

--- client/src/containers/Home/index.tsx
+++ client/src/containers/Home/index.tsx
@@ -5,7 +5,10 @@ import { MenuItems } from 'common/enums';
 const Home = (): JSX.Element => {
   return (
     <PageComponent selectedMenuItemNumber={MenuItems.Home}>
-      <div>Home</div>
+      <div style={{marginTop: "100px", display: "flex", fontSize: "2em", flexDirection: "column"}} >
+        <h1 style={{ marginBottom: 0, fontSize: "3em"}}>PCForge</h1>
+        <h2 style={{ marginTop: 0 }}>Hand-forged gaming setups</h2>
+      </div>
     </PageComponent>
   );
 };
