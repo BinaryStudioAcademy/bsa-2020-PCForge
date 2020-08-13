0ef90ad574324652201d487d0d24e9f0ffbb9316
Vismmax <vismmax@gmail.com>
2020-08-13 05:46:42 +0300

    Added reducer

--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { AuthReducer } from 'containers/Auth/reducer';
+import builderReducer from 'containers/BuilderPage/reducer';
 
 const RootReducer = combineReducers({
   auth: AuthReducer,
+  setup: builderReducer,
 });
 
 export default RootReducer;
