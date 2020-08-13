c91ed512ca915591e32cc5dbacf5923eef720174
Vismmax <vismmax@gmail.com>
2020-08-13 05:45:36 +0300

    Added sagas

--- client/src/redux/rootSaga.ts
+++ client/src/redux/rootSaga.ts
@@ -1,7 +1,6 @@
 import { all } from 'redux-saga/effects';
+import builderSagas from 'containers/BuilderPage/sagas';
 
 export default function* rootSaga() {
-  yield all([
-    // Define your sagas here
-  ]);
+  yield all([builderSagas()]);
 }
