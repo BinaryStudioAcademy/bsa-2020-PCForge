7e57d3515905745625bf3f8773f658ec2ed928b4
Lesiv Alexandr <lesivolexandr@gmail.com>
2020-08-13 10:46:13 +0300

    Update auth service routes

--- client/src/api/services/auth.service.ts
+++ client/src/api/services/auth.service.ts
@@ -1,11 +1,11 @@
 import { User } from 'common/models/user';
 import { IAuthPayload } from 'containers/Auth/interfaces';
 import { setToken } from 'helpers/tokenHelper';
-import api from 'helpers/webApiHelper';
+import api from 'api/webApiHelper';
 
 export class AuthService {
   async login(data: IAuthPayload): Promise<User> {
-    const apiRoute = 'http://localhost:5001/api/auth/login';
+    const apiRoute = '/auth/login';
 
     const response = await api.post(apiRoute, data);
     setToken(response.token);
@@ -14,7 +14,7 @@ export class AuthService {
   }
 
   async createUser(data: IAuthPayload): Promise<User> {
-    const apiRoute = 'http://localhost:5001/api/users';
+    const apiRoute = '/users';
     const response = await api.post(apiRoute, data);
     return response;
   }
