4fe47db602205a5415092ea555d11fe798adfa3a
Evgeniy <evgeny.kasyan@outlook.com>
2020-08-14 04:33:50 +0300

    Edit service for Image Uploading

--- server/src/api/plugins/imageUpload.ts
+++
@@ -1,7 +0,0 @@
-import fp from 'fastify-plugin';
-import fastifyMultipart from 'fastify-multipart';
-
-export default fp((fastify, opts, next) => {
-  fastify.register(fastifyMultipart);
-  next();
-});
--- server/src/api/routes/imageUpload.router.ts
+++ server/src/api/routes/imageUpload.router.ts
@@ -1,31 +1,19 @@
 import { FastifyInstance } from 'fastify';
 import { FastifyNext, FastifyOptions } from './fastifyTypes';
-import { resolve } from 'bluebird';
 
 export function router(fastify: FastifyInstance, opts: FastifyOptions, next: FastifyNext): void {
+  //Example how to use image Upload
   const { UploadImageService } = fastify.services;
   const singleUpload = UploadImageService.single('image');
 
   fastify.post('/image', { preHandler: singleUpload }, (request, response) => {
     singleUpload(request, response, (error) => {
       if (error) {
-        console.log('errors', error);
         response.send({ error: error });
-      } else {
-        // If File not found
-        if (request.file === undefined) {
-          console.log('Error: No File Selected!');
-          response.send({ error: 'Error: No File Selected' });
-        } else {
-          // If Success
-          const imageName = request.file.key;
-          const imageLocation = request.file.location;
-          // Save the file name into database into profile model
-          response.send({
-            image: imageName,
-            location: imageLocation,
-          });
-        }
+      }
+      // If File not found
+      if (request.file === undefined) {
+        response.send({ error: 'Error: No File Selected' });
       }
       response.send({ imageUrl: request.file.location });
     });
