adc4cf2f501211582cfef4273fb325b91578aa02
Vismmax <vismmax@gmail.com>
2020-08-16 11:15:10 +0300

    Rename functions

--- client/src/helpers/setupHelper.ts
+++ client/src/helpers/setupHelper.ts
@@ -2,17 +2,17 @@ import { TypeSetup } from 'containers/BuilderPage/reducer';
 
 const keySetup = 'setup';
 
-export const getSetup = async (): Promise<TypeSetup | null> => {
+export const getLocalSetup = async (): Promise<TypeSetup | null> => {
   const setupString = window.localStorage.getItem(keySetup);
   return setupString ? JSON.parse(setupString) : setupString;
 };
 
-export const setSetup = async (setup: TypeSetup): Promise<void> => {
+export const setLocalSetup = async (setup: TypeSetup): Promise<void> => {
   const setupString = JSON.stringify(setup);
   window.localStorage.setItem(keySetup, setupString);
 };
 
-export const updateSetup = async (setup: TypeSetup): Promise<void> => {
+export const updateLocalSetup = async (setup: TypeSetup): Promise<void> => {
   const setupString = window.localStorage.getItem(keySetup);
   const localSetup = setupString ? JSON.parse(setupString) : {};
   const newSetup = { ...localSetup, ...setup };
@@ -20,10 +20,10 @@ export const updateSetup = async (setup: TypeSetup): Promise<void> => {
   window.localStorage.setItem(keySetup, newSetupString);
 };
 
-export const clearSetup = async (): Promise<void> => {
+export const clearLocalSetup = async (): Promise<void> => {
   window.localStorage.setItem(keySetup, JSON.stringify({}));
 };
 
-export const deleteSetup = async (): Promise<void> => {
+export const deleteLocalSetup = async (): Promise<void> => {
   window.localStorage.removeItem(keySetup);
 };
