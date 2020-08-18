ed8c4b60fdb69a3e445cb7716c44ff18f49e929b
Cool <yuliakulinichsoft@gmail.com>
2020-08-18 17:23:22 +0300

    Add using actions

--- client/src/containers/Auth/registrationSagas.ts
+++ client/src/containers/Auth/registrationSagas.ts
@@ -1,31 +1,25 @@
 import { authService } from 'api/services/auth.service';
 import { IRegPayload } from './interfaces';
 import { User } from 'common/models/user';
-import { call, put, takeEvery, all } from 'redux-saga/effects';
-import {
-  AUTH_REGISTER_REQUEST,
-  AUTH_LOADING_STATUS,
-  AUTH_REGISTRATION_SUCCESS,
-  AUTH_REGISTRATION_ERROR,
-  registerRequestAction,
-} from './actionTypes';
+import { call, put, takeLatest, all } from 'redux-saga/effects';
+import { AUTH_REGISTER_REQUEST, registerRequestAction } from './actionTypes';
+import { changeLoadingStatus, validationError, registered } from './actions';
 
 export function* registerUser(action: registerRequestAction) {
   const newUser = { ...action.payload };
-  console.log(newUser);
   try {
-    yield put({ type: AUTH_LOADING_STATUS, payload: { isLoading: true } });
+    yield put(changeLoadingStatus(true));
     const data: User = yield call<(data: IRegPayload) => void>(authService.register, newUser);
-    yield put({ type: AUTH_REGISTRATION_SUCCESS, payload: { isRegistration: false } });
+    yield put(registered(false));
   } catch (error) {
-    yield put({ type: AUTH_REGISTRATION_ERROR, payload: { message: error.message } });
+    yield put(validationError(error));
   } finally {
-    yield put({ type: AUTH_LOADING_STATUS, payload: { isLoading: false } });
+    yield put(changeLoadingStatus(false));
   }
 }
 
 function* watchRegisterUser() {
-  yield takeEvery(AUTH_REGISTER_REQUEST, registerUser);
+  yield takeLatest(AUTH_REGISTER_REQUEST, registerUser);
 }
 
 export default function* registrationSaga() {
