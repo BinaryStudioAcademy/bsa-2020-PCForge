ee1b75a958404b630a215278bb17c3878c61ca8f
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-18 18:16:06 +0300

    client: connect SetupChart container to root saga and root reducer

--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -4,6 +4,7 @@ import NotificationReducer from 'containers/Notification/logic/reducer';
 import UserReducer from 'containers/UserPage/logic/reducer';
 import builderReducer from 'containers/BuilderPage/reducer';
 import { QuickMatcherReducer } from 'containers/QuickMatcher/reducer';
+import { SetupChartReducer } from 'containers/Chart/reducer';
 
 const RootReducer = combineReducers({
   auth: AuthReducer,
@@ -11,6 +12,7 @@ const RootReducer = combineReducers({
   setup: builderReducer,
   quickMatcher: QuickMatcherReducer,
   notification: NotificationReducer,
+  setupChart: SetupChartReducer,
 });
 
 export default RootReducer;
--- client/src/redux/rootSaga.ts
+++ client/src/redux/rootSaga.ts
@@ -3,7 +3,8 @@ import userSagas from 'containers/UserPage/logic/sagas';
 import builderSagas from 'containers/BuilderPage/sagas';
 import authSagas from 'containers/Auth/sagas';
 import quickMatcherSagas from 'containers/QuickMatcher/sagas';
+import setupChartSagas from 'containers/Chart/sagas';
 
 export default function* rootSaga() {
-  yield all([authSagas(), userSagas(), builderSagas(), quickMatcherSagas()]);
+  yield all([authSagas(), userSagas(), builderSagas(), quickMatcherSagas(), setupChartSagas()]);
 }
