acab03feb0f11d0e1195944055c4cdaf94541f97
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-19 10:56:23 +0300

    client: performance charts in ChartComponent now works and scales

--- client/src/components/ChartComponents/PerfomanceReport/index.tsx
+++ client/src/components/ChartComponents/PerfomanceReport/index.tsx
@@ -1,10 +1,18 @@
+import { width } from '@material-ui/system';
+import { roundToNearest } from 'common/helpers/math.helper';
 import { Setup } from 'common/models/setup';
 import { IReport } from 'common/models/setupPerformance';
 import React, { useState } from 'react';
 import sharedStyles from '../styles.module.scss';
 import styles from './styles.module.scss';
 
-interface RecommendedRequirement {
+interface IRequirement {
+  cpu: number;
+  gpu: number;
+  ram: number;
+}
+
+interface UIRequirement {
   id: number;
   title: string;
 }
@@ -20,20 +28,25 @@ interface Props {
 }
 
 const GameMatcherPerformanceReport: React.FC<Props> = ({ setup, report }): JSX.Element => {
-  const [selectedRequirement, setSelectedRequirement] = useState<number>(1);
+  const RECOMMENDED_REQUIREMENT_ID = 1;
+  const MINIMAL_REQUIREMENT_ID = 2;
+  const MAXIMUM_REPORT_VALUE = Math.max(
+    roundToNearest(report.minimal.cpu, 100),
+    roundToNearest(report.minimal.gpu, 100),
+    roundToNearest(report.minimal.ram, 100),
+    roundToNearest(report.recommended.cpu, 100),
+    roundToNearest(report.recommended.gpu, 100),
+    roundToNearest(report.recommended.ram, 100)
+  );
+  const REPORT_STEP = roundToNearest(MAXIMUM_REPORT_VALUE / 10, 10);
+  const [selectedRequirement, setSelectedRequirement] = useState<number>(RECOMMENDED_REQUIREMENT_ID);
   const values = [];
-  for (let i = 0; i <= 250; i += 25) {
+  for (let i = 0; i <= MAXIMUM_REPORT_VALUE; i += REPORT_STEP) {
     values.push(i);
   }
-  const recommendedRequirements = [
-    { id: 1, title: 'Recommended requirement' },
-    { id: 2, title: 'Minimal requirement' },
-  ];
-
-  const performanceIndicators = [
-    { name: 'CPU', value: 40 },
-    { name: 'GPU', value: 21 },
-    { name: 'RAM', value: 81 },
+  const requirements: UIRequirement[] = [
+    { id: RECOMMENDED_REQUIREMENT_ID, title: 'Recommended requirement' },
+    { id: MINIMAL_REQUIREMENT_ID, title: 'Minimal requirement' },
   ];
 
   const componentItems = [
@@ -42,7 +55,7 @@ const GameMatcherPerformanceReport: React.FC<Props> = ({ setup, report }): JSX.E
     { title: 'RAM', description: `${setup.ram.memorySize}GB` },
   ];
 
-  const RecommendedRequirement = (req: RecommendedRequirement) => {
+  const getRequirementDiagram = (req: UIRequirement) => {
     const isActive = req.id === selectedRequirement;
 
     return (
@@ -53,20 +66,40 @@ const GameMatcherPerformanceReport: React.FC<Props> = ({ setup, report }): JSX.E
     );
   };
 
-  const PerformanceIndicator = (ind: PerformanceIndicatorItem) => (
-    <div className={styles.performanceGraphItem} key={ind.name}>
-      <span className={styles.performanceIndicatorHeader}>{ind.name}</span>
+  const getScaledWidth = (width: number): number => {
+    const totalSteps = MAXIMUM_REPORT_VALUE / REPORT_STEP;
+    const currentStep = width / REPORT_STEP;
+    return (currentStep / totalSteps) * 100;
+  };
+
+  const getPerformanceIndicator = (name: string, value: number) => (
+    <div className={styles.performanceGraphItem} key={name}>
+      <span className={styles.performanceIndicatorHeader}>{name}</span>
       <div className={styles.performanceGraphCentralLine}></div>
-      <div className={styles.performanceIndicator} style={{ width: ind.value + '%' }}></div>
+      <div className={styles.performanceIndicator} style={{ width: getScaledWidth(value) + '%' }}></div>
     </div>
   );
+
+  const getPerformanceIndicators = (req: IRequirement) => {
+    console.log(req.cpu);
+    return (
+      <>
+        {getPerformanceIndicator('CPU', req.cpu)}
+        {getPerformanceIndicator('GPU', req.gpu)} {getPerformanceIndicator('RAM', req.ram)}
+      </>
+    );
+  };
 
   return (
     <section>
       <h2 className={sharedStyles.mainHeader}>Performance Report</h2>
-      <div className={styles.requirementVariants}>{recommendedRequirements.map(RecommendedRequirement)}</div>
+      <div className={styles.requirementVariants}>{requirements.map(getRequirementDiagram)}</div>
 
-      <div className={styles.performanceGraph}>{performanceIndicators.map(PerformanceIndicator)}</div>
+      {selectedRequirement === RECOMMENDED_REQUIREMENT_ID ? (
+        <div className={styles.performanceGraph}>{getPerformanceIndicators(report.recommended)}</div>
+      ) : (
+        <div className={styles.performanceGraph}>{getPerformanceIndicators(report.minimal)}</div>
+      )}
       <div className={styles.indicatorValues}>
         {values.map((value) => (
           <span key={value} className={styles.indicatorValue}>
