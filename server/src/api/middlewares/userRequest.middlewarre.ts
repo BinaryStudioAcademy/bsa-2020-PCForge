c7b4f5e8196a19f466923ce012ef1da33d6dd761
Evgeniy <evgeny.kasyan@outlook.com>
2020-08-20 05:43:26 +0300

    Fix userRequestMiddleware

--- server/src/api/middlewares/userRequest.middlewarre.ts
+++ server/src/api/middlewares/userRequest.middlewarre.ts
@@ -2,16 +2,17 @@ import { FastifyReply, FastifyRequest, FastifyInstance } from 'fastify';
 import { FastifyDone } from '../routes/fastifyTypes';
 import { UserAttributes } from '../../data/models/user';
 type CustomRequest = FastifyRequest<{
-  Body: { token: string; toke: string };
+  Body: { token: string };
 }> & { user: UserAttributes };
 
 export const userRequestMiddleware = (fastify: FastifyInstance) => {
   return async (request: CustomRequest, reply: FastifyReply, done: FastifyDone): Promise<void> => {
+    console.log('userRequestMiddleware -> request.body', request);
     const { token } = request.body;
+
     fastify.jwt.verify(token, (err, decoded) => {
       request.user = decoded.user;
     });
-    done();
   };
 };
 
