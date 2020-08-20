3d59685842d62c5fcf596326acbfe67e5b79a6e9
Vismmax <vismmax@gmail.com>
2020-08-20 08:53:49 +0300

    Edited ui

--- client/src/components/BuilderPage/Search/index.tsx
+++ client/src/components/BuilderPage/Search/index.tsx
@@ -15,23 +15,17 @@ const Search = ({ value, onChange, className = '' }: Props): ReactElement => {
     <Box className={className}>
       <FormControl className={styles.search} fullWidth={true} variant="outlined" size="small">
         <OutlinedInput
-          className={className}
-          classes={{
-            root: styles.inputWrap,
-            input: styles.input,
-            focused: styles.focused,
-          }}
+          className={styles.inputWrap}
           fullWidth={true}
           placeholder="Search"
           value={value}
           onChange={(ev) => onChange(ev.target.value)}
-          // autoComplete={'asda aaaaaa adfrw'}
           endAdornment={
-            <InputAdornment classes={{ root: styles.icon }} position="end">
+            <InputAdornment className={styles.icon} position="end">
               {value ? (
-                <CancelIcon classes={{ root: styles.iconFill }} onClick={() => onChange('')} />
+                <CancelIcon className={styles.iconFill} onClick={() => onChange('')} />
               ) : (
-                <SearchIcon classes={{ root: styles.iconFill }} />
+                <SearchIcon className={styles.iconFill} />
               )}
             </InputAdornment>
           }
