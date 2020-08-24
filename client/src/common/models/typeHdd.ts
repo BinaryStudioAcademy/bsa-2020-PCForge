edf15b9272149bcf20edce02a049f069209fe172
Vismmax <vismmax@gmail.com>
2020-08-24 11:15:56 +0300

    Added types hdd, ssd

---
+++ client/src/common/models/typeHdd.ts
@@ -0,0 +1,11 @@
+export type TypeHdd = {
+  id: number;
+  name: string;
+  capacity: number;
+  size: number;
+  sata: number;
+  rpm: number;
+  ram: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
