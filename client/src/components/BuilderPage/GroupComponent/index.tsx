152a5049b6178540991279c754ee4bffe179b728
Vismmax <vismmax@gmail.com>
2020-08-26 14:36:26 +0300

    Removed log

--- client/src/components/BuilderPage/GroupComponent/index.tsx
+++ client/src/components/BuilderPage/GroupComponent/index.tsx
@@ -138,7 +138,6 @@ const GroupComponent = ({
       /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
       // @ts-ignore
       specifications={SpecificationComponent[groupName]({ component })}
-      // onAddComponent={() => onAddComponent(groupName, component.id)}
       onAddComponent={() => onAddComponentHandler(component.id)}
     />
   ));
