8f9deca7b5edfefc80933c332ab8239fee420044
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-27 12:25:54 +0300

    client: fix linter errors

--- client/src/containers/EmailVerificationPage/actions.ts
+++ client/src/containers/EmailVerificationPage/actions.ts
@@ -1,6 +1,6 @@
-import { VERIFY_EMAIL } from "./actionTypes";
+import { VERIFY_EMAIL } from './actionTypes';
 
-export const verifyEmail = (payload: {token: string}) => ({
+export const verifyEmail = (payload: { token: string }) => ({
   type: VERIFY_EMAIL,
-  payload
-})
+  payload,
+});
