04142eecdc269721ed5ecf2a00af06f05d295667
Maryna <marynavatsko@gmail.com>
2020-08-27 23:58:32 +0300

    improved home page layout for large screens

--- client/src/containers/Home/components/CardsDisplay/index.tsx
+++ client/src/containers/Home/components/CardsDisplay/index.tsx
@@ -1,44 +1,26 @@
 import React from 'react';
 import SetupCard, { SetupCardProps } from 'components/SquareSetupCard';
-import { getIcon } from 'common/helpers/icon.helper';
+import ButtonCard from '../ButtonCard';
+
 import styles from './styles.module.scss';
 
 interface ICardDisplayProps {
   setups: SetupCardProps[];
+  className?: string;
+  showButton?: boolean;
 }
 
 const CardDisplay: React.FC<ICardDisplayProps> = (props) => {
-  const { setups } = props;
-  const topSetup = setups[0];
-  const ordinarySetups = setups.filter((setup, index) => {
-    return index !== 0;
-  });
+  const { showButton, setups, className } = props;
+ 
 
   const generateKey = (pre: string, index: number) => {
     return `${pre}_${new Date().getTime()}_${index}`;
   };
 
   return (
-    <div className={styles.contentWrapper}>
-      <h2 className={styles.buildsHeading}>Most popular builds</h2>
-      <div className={styles.cardDisplay}>
-        <div className={styles.topSetup}>
-          <SetupCard
-            id={topSetup.id}
-            title={topSetup.title}
-            description={topSetup.description}
-            image={topSetup.image}
-            cpu={topSetup.cpu}
-            gpu={topSetup.gpu}
-            motherboard={topSetup.motherboard}
-            ram={topSetup.ram}
-            powerSupply={topSetup.powerSupply}
-            big
-            key={generateKey(topSetup.title, 0)}
-          />
-        </div>
-        <div className={styles.smallerCards}>
-          {ordinarySetups.map((setup, index) => {
+        <div>
+          {setups.map((setup, index) => {
             return (
               <SetupCard
                 id={setup.id}
@@ -54,15 +36,9 @@ const CardDisplay: React.FC<ICardDisplayProps> = (props) => {
               />
             );
           })}
-          <div className={styles.cardButton}>
-            {getIcon('Visibility')}
-            <p>Show all computers</p>
-
-            {getIcon('ArrowForward')}
-          </div>
-        </div>
+          {showButton && <ButtonCard />}
       </div>
-    </div>
+   
   );
 };
 
