ca83d64076bd5119c999f8ef3e64708e5b23e24a
Evgeniy <evgeny.kasyan@outlook.com>
2020-08-27 03:18:20 +0300

    Redirect to setup when setup is saved

--- client/src/containers/BuilderPage/sagas.ts
+++ client/src/containers/BuilderPage/sagas.ts
@@ -1,5 +1,7 @@
 import { call, put, takeEvery, all, select } from 'redux-saga/effects';
 import { AnyAction } from 'redux';
+import { push } from 'react-router-redux';
+import history from '../../browserHistory';
 import {
   ADD_COMPONENT_TO_SETUP,
   FETCH_COMPONENT_SUCCESS,
@@ -93,6 +95,7 @@ export function* saveSetup(action: AnyAction) {
     const data = action.payload.data;
     data.image = yield call(uploadImage, action.payload.image);
     const savedSetup = yield call(createSetup, data);
+    history.push(`setup/${savedSetup.id}`);
   } catch (error) {
     console.log(error);
   }
