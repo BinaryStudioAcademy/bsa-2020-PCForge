b4667aeccaad2220d29071cb7d92e51528cce6ce
Yaroslaww1 <yaroslaww1@gmail.com>
2020-08-31 22:26:27 +0300

    client: add @deprecated tag to old notifications realization

--- client/src/containers/Notification @deprecated/logic/actionTypes.ts
+++ client/src/containers/Notification @deprecated/logic/actionTypes.ts
@@ -1,4 +1,4 @@
-import { MessageSeverity } from './../components/Message';
+import { MessageSeverity } from '../components/Message';
 
 export const SHOW_NOTIFICATION = 'SHOW_NOTIFICATION';
 export const HIDE_NOTIFICATION = 'HIDE_NOTIFICATION';
