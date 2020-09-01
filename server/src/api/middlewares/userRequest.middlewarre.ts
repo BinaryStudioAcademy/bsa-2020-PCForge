c83769aadc1739a55c9a3638481caee525e4dee9
Yaroslaww1 <yaroslaww1@gmail.com>
2020-09-01 19:52:18 +0300

    server: id from token must be equal to id from params

--- server/src/api/middlewares/userRequest.middlewarre.ts
+++ server/src/api/middlewares/userRequest.middlewarre.ts
@@ -2,7 +2,8 @@ import { FastifyReply, FastifyRequest, FastifyInstance } from 'fastify';
 import { FastifyDone } from '../routes/fastifyTypes';
 import { UserAttributes } from '../../data/models/user';
 import { OAuth2Client } from 'google-auth-library';
-type CustomRequest = FastifyRequest & { user: UserAttributes };
+import { triggerServerError } from '../../helpers/global.helper';
+type CustomRequest = FastifyRequest<{ Params: { id: string } }> & { user: UserAttributes };
 
 const oAuth2Client = new OAuth2Client(
   process.env.GOOGLE_OAUTH_CLIENT_ID,
@@ -15,7 +16,7 @@ export const userRequestMiddleware = (fastify: FastifyInstance) => {
   return async (request: CustomRequest, reply: FastifyReply, done: FastifyDone): Promise<void> => {
     const token = request.headers?.authorization?.replace('Bearer ', '') || '';
     if (!token) {
-      return;
+      triggerServerError(`Authorization token is not defined`, 400);
     }
 
     let decodedData: undefined | { user: UserAttributes };
@@ -26,27 +27,24 @@ export const userRequestMiddleware = (fastify: FastifyInstance) => {
       }
     });
 
+    if (decodedData && request.params.id) {
+      if (decodedData.user.id !== parseInt(request.params.id, 10)) {
+        triggerServerError(`Authorized user has different id from querystring userId`, 400);
+        console.log('AAA');
+      }
+    }
+
     if (!decodedData) {
       try {
         const userData = (await oAuth2Client.verifyIdToken({ idToken: token })).getPayload();
-        try {
-          const user = await UserService.getByEmail(userData.email);
-          request.user = user;
-        } catch (err) {
-          return;
+        const user = await UserService.getByEmail(userData.email);
+        if (!user) {
+          triggerServerError(`User with email ${userData.email} is not defined`, 400);
         }
-      } catch (e) {
-        return;
+        request.user = user;
+      } catch (err) {
+        triggerServerError(err, 400);
       }
     }
   };
 };
-
-//UsageExample
-// fastify.post(
-//   '/route',
-//   {
-//     preHandler: [userRequestMiddleware(fastify)],
-//   },
-//   handler()
-// )
