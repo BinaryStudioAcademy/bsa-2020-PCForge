5f806dad615444b6ef3b7b66cc12124849d3057d
Vismmax <vismmax@gmail.com>
2020-09-01 10:33:36 +0300

    Added PublicRoute

---
+++ client/src/containers/PublicRoute/index.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginByTokenRequest } from 'containers/Auth/actions';
+import { Routes } from 'common/enums/routes';
+import { RootState } from 'redux/rootReducer';
+import * as Sentry from '@sentry/react';
+
+type PropsType = {
+  component: any;
+  exact?: boolean;
+  path: Routes | string;
+  className?: string;
+};
+
+const SentryRoute = Sentry.withSentryRouting(Route);
+
+const PublicRoute = ({ component: Component, ...rest }: PropsType) => {
+  const isAuthorized = useSelector((state: RootState) => !!state.auth.user?.id);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!isAuthorized) dispatch(loginByTokenRequest());
+  }, []);
+
+  return (
+    <SentryRoute
+      {...rest}
+      render={(props) => (isAuthorized ? <Redirect to={Routes.DEFAULT} /> : <Component {...props} />)}
+    />
+  );
+};
+
+export default PublicRoute;
