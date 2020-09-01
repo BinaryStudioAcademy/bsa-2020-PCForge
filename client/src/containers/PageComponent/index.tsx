90f92fefe95f8d9849a3c0db005df4cec9d1c7da
Vismmax <vismmax@gmail.com>
2020-09-01 10:35:30 +0300

    Updated PageComponent

--- client/src/containers/PageComponent/index.tsx
+++ client/src/containers/PageComponent/index.tsx
@@ -1,16 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import NavigationBar from '../../components/NavigationBar';
-import Footer from '../../components/Footer';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import NavigationBar from 'components/NavigationBar';
+import Footer from 'components/Footer';
 import classes from './styles.module.scss';
-import { Redirect } from 'react-router-dom';
-import { MenuItems, Routes } from 'common/enums';
+import { MenuItems } from 'common/enums';
 import Spinner from 'components/Spinner';
 import TopBar from 'containers/TopBar';
-import { clearToken } from 'helpers/tokenHelper';
-import { useDispatch } from 'react-redux';
-import { loginRequestSuccess } from '../Auth/actions';
-import { authService } from 'api/services/auth.service';
-import * as Sentry from '@sentry/react';
+import { RootState } from 'redux/rootReducer';
 
 interface IProps {
   selectedMenuItemNumber?: MenuItems;
@@ -18,40 +14,13 @@ interface IProps {
 }
 
 const PageComponent: React.FC<IProps> = ({ selectedMenuItemNumber, children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoading, setLoading] = useState(true);
-  const [isAdmin, setIsAdmin] = useState(false);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    checkIsUserAuthenticated();
-  }, []);
-
-  const checkIsUserAuthenticated = async () => {
-    try {
-      const isAuthenticated = await authService.getUserByToken();
-      setIsAuthenticated(isAuthenticated.logged_in);
-      if (!isAuthenticated.logged_in) {
-        await clearToken();
-      }
-      if (isAuthenticated.user) {
-        setIsAdmin(isAuthenticated.user.isAdmin);
-        dispatch(loginRequestSuccess(isAuthenticated.user));
-      }
-    } catch (e) {
-      // message
-    } finally {
-      setLoading(false);
-    }
-  };
+  const isAdmin = useSelector((state: RootState) => state.auth.user?.isAdmin);
+  const isLoading = useSelector((state: RootState) => state.auth.isLoading);
 
   return isLoading ? (
     <div className={classes.spinnerWrapper}>
       <Spinner />
     </div>
-  ) : !isAuthenticated ? (
-    <Redirect to={Routes.LOGIN} />
   ) : (
     <div className={classes.rootComponent}>
       <TopBar />
