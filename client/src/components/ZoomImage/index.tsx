2ae290bc3b18e351c9ad84b85b2d301eac720fee
Lesiv Olexandr <lesivolexandr@gmail.com>
2020-09-02 19:44:36 +0300

    Fix images

--- client/src/components/ZoomImage/index.tsx
+++ client/src/components/ZoomImage/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, MouseEvent } from 'react';
+import Zoom from 'react-medium-image-zoom';
+import { Magnifier, SideBySideMagnifier } from 'react-image-magnifiers';
 import styles from './styles.module.scss';
 
 interface Props {
@@ -11,29 +13,15 @@ interface Props {
 const ZoomImage: React.FC<Props> = (props): JSX.Element => {
   const { src, alt, rootClassName } = props;
 
-  const [imgPosition, setPosition] = useState('0% 0%');
-  const [defaultImage, setDefaultImage] = useState<string | null>(null);
-
-  const handleMouseMove = (e: MouseEvent<HTMLElement>) => {
-    const { left, top, width, height } = (e.target as HTMLElement).getBoundingClientRect();
-    const x = ((e.clientX - left) / width) * 100;
-    const y = ((e.clientY - top) / height) * 100;
-    setPosition(`${x}% ${y}%`);
-  };
-
-  const onImageLoadError = () => {
-    if (!defaultImage && props.fallbackImage) {
-      setDefaultImage(props.fallbackImage!);
-    }
-  };
   return (
-    <figure
+    <SideBySideMagnifier
       className={[styles.imageWrapper, rootClassName ? rootClassName : ''].join(' ')}
-      onMouseMove={handleMouseMove}
-      style={{ backgroundPosition: imgPosition, backgroundImage: `url(${defaultImage || src})` }}
-    >
-      <img src={defaultImage || src} alt={alt} className={styles.image} onError={onImageLoadError} />
-    </figure>
+      imageSrc={src}
+      imageAlt={alt}
+      overlayBackgroundColor={'#3C444D'}
+      fillAvailableSpace={true}
+      alwaysInPlace={true}
+    ></SideBySideMagnifier>
   );
 };
 
