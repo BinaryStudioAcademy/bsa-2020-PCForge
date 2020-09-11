3fb4f6fe23054b811f1618d659358775536d438d
Yaroslaww1 <yaroslaww1@gmail.com>
2020-09-11 16:34:21 +0300

    server: started working on hardware stats

--- server/src/api/services/statistics.service.ts
+++ server/src/api/services/statistics.service.ts
@@ -1,6 +1,17 @@
+import { CpuAttributes } from '../../data/models/cpu';
+import { GpuAttributes } from '../../data/models/gpu';
+import { HddAttributes } from '../../data/models/hdd';
+import { MotherboardAttributes } from '../../data/models/motherboard';
+import { RamAttributes } from '../../data/models/ram';
+import { SsdAttributes } from '../../data/models/ssd';
 import { StatisticHardwareAttributes, StatisticUserSetupsAttributes } from '../../data/models/statistics';
 import { SetupRepository } from '../../data/repositories/setup.repository';
 import { UserRepository } from '../../data/repositories/user.repository';
+
+interface IHardware {
+  type: 'cpu' | 'gpu' | 'ram' | 'ssd' | 'hdd' | 'motherboard';
+  hardware: CpuAttributes | GpuAttributes | RamAttributes | SsdAttributes | HddAttributes | MotherboardAttributes;
+}
 
 export class StatisticService {
   constructor(private setupRepository: SetupRepository, private userRepository: UserRepository) {}
@@ -22,10 +33,19 @@ export class StatisticService {
     return statistics;
   }
 
-  // async getMostUsedHardware(): Promise<StatisticHardwareAttributes> {
-  //   const setups = await this se
-  //   //const staticData = await
-  // }
+  async getMostUsedHardware(): Promise<StatisticHardwareAttributes> {
+    const setups = (await this.setupRepository.getAll({}, { from: 0, count: 1000 })).data;
+    const hardwareMap = new Map<IHardware, number>();
+    for (const setup of setups) {
+      const cpu = { type: 'cpu', hardware: setup.cpu };
+      const gpu = { type: 'gpu', hardware: setup.gpu };
+      const ram = { type: 'ram', hardware: setup.ram };
+      const motherboard = { type: 'motherboard', hardware: setup.motherboard };
+      const ssd = { type: 'ssd', hardware: setup.ssd };
+      const hdd = { type: 'hdd', hardware: setup.hdd };
+    }
+    //const staticData = await
+  }
   /*async getMostActiveUsers(): Promise<StatisticModel> {
 
   }
