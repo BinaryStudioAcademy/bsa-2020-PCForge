7073784dde5765d874d80275e489b11429d934a9
Evgeniy <evgeny.kasyan@outlook.com>
2020-09-11 15:17:52 +0300

    Open selected hardware from global search

--- client/src/containers/HardwaresPage/actions.ts
+++ client/src/containers/HardwaresPage/actions.ts
@@ -1,7 +1,13 @@
-import { hardwaresActionTypes, HARDWARES_GET_HARDWARES } from './actionTypes';
+import { hardwaresActionTypes, HARDWARES_GET_HARDWARES, SET_SELECTED_HARDWARE } from './actionTypes';
 import { HardwareSearchPayload } from './interfaces';
+import { ReactText } from 'react';
 
 export const getHardwares = (payload: HardwareSearchPayload): hardwaresActionTypes => ({
   type: HARDWARES_GET_HARDWARES,
+  payload,
+});
+
+export const setHardware = (payload: Record<string, ReactText> | null): hardwaresActionTypes => ({
+  type: SET_SELECTED_HARDWARE,
   payload,
 });
